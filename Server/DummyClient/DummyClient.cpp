bb00788431d2d0f4dc5279fea31726956b7543bb
thescanthis <thescanthis@gmail.com>
2024-06-04 21:19:03 +0900

    Socket Programing Chapter1

--- Server/DummyClient/DummyClient.cpp
+++ Server/DummyClient/DummyClient.cpp
@@ -1,68 +1,74 @@
 ﻿#include "pch.h"
 #include <iostream>
 
+//
 #include <winsock2.h>
 #include <mswsock.h>
 #include <ws2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
 
-int main()
+bool Socket_Error(SOCKET clientSocket)
 {
-	// 윈속 초기화 (ws2_32 라이브러리 초기화)
-	// 관련 정보가 wsaData에 채워짐
-	WSAData wsaData;
-	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-		return 0;
-
-	// ad : Address Family (AF_INET = IPv4, AF_INET6 = IPv6)
-	// type : TCP(SOCK_STREAM) vs UDP(SOCK_DGRAM)
-	// protocol : 0
-	// return : descriptor
-	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
 	if (clientSocket == INVALID_SOCKET)
 	{
 		int32 errCode = ::WSAGetLastError();
-		cout << "Socket ErrorCode : " << errCode << endl;
-		return 0;
+		cout << "Socket ErrorCode" << errCode << '\n';
+		return false;
 	}
+	return true;
+}
 
-	// 연결할 목적지는? (IP주소 + Port) -> XX 아파트 YY 호
-	SOCKADDR_IN serverAddr; // IPv4
+void SockAddr_In_Init(SOCKADDR_IN& serverAddr)
+{
+	//연결할 목적지 ? (IP주소 + Port)
 	::memset(&serverAddr, 0, sizeof(serverAddr));
 	serverAddr.sin_family = AF_INET;
-	//serverAddr.sin_addr.s_addr = ::inet_addr("127.0.0.1"); << deprecated
+	//serverAddr.sin_addr.s_addr = ::inet_addr("127.0.0.1");
 	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
-	serverAddr.sin_port = ::htons(7777); // 80 : HTTP
-	// host to network short
-	// Little-Endian vs Big-Endian
-	// ex) 0x12345678 4바이트 정수
-	// low [0x78][0x56][0x34][0x12] high < little
-	// low [0x12][0x34][0x56][0x78] high < big = network
+	serverAddr.sin_port = ::htons(7777);
+	//host to network short = 내컴퓨터에서 네트워크에 알맞게 고침
+	//Little-Enddian vs Big-Endian
+	//[0x78][0x56][0x34][0x12] = little
+	//[0x12][0x34][0x56][0x78] = big = network 선호
+}
 
-	if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
-	{
-		int32 errCode = ::WSAGetLastError();
-		cout << "Connect ErrorCode : " << errCode << endl;
+int main()
+{
+	//Winsock 초기화 서버와 클라 동일함 초기화부분
+	//관련 정보가 wsaData에 채워짐
+	WSAData wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData))
 		return 0;
+
+#if 주석
+	클라와 서버 둘다 같음.
+	//1. 소켓을 만들어야함.
+	//소문자는 리눅스에서도 사용할수있을 가능성이 높음.
+	//ad :AF_INET = IPv4,AF_INET6=IPv6
+	//TCP = SOCK_STREAM,UDP = SOCK_DGRAM
+	//protocol = 0
+	//return : descriptor
+#endif
+	SOCKET clientSocket = ::socket(AF_INET,SOCK_STREAM,0);
+	Socket_Error(clientSocket);
+
+	SOCKADDR_IN serverAddr; // IPv4
+	SockAddr_In_Init(serverAddr);
+
+	if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr))==SOCKET_ERROR)
+	{
+		Socket_Error(clientSocket);
 	}
 
-	// ---------------------------
-	// 연결 성공! 이제부터 데이터 송수신 가능!
-
-	cout << "Connected To Server!" << endl;
+	cout << "Connected To Server!" << '\n';
 
 	while (true)
 	{
-		// TODO
-
+		//TODO
 		this_thread::sleep_for(1s);
 	}
 
-	// ---------------------------
-
-	// 소켓 리소스 반환
 	::closesocket(clientSocket);
-
-	// 윈속 종료
 	::WSACleanup();
+	return 0;
 }
--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -7,12 +7,74 @@
 #include <future>
 #include "ThreadManager.h"
 
+
 #include <winsock2.h>
 #include <mswsock.h>
 #include <ws2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
 
+bool Socket_Error(SOCKET listenSocket)
+{
+	if (listenSocket == INVALID_SOCKET)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Socket ErrorCode" << errCode << '\n';
+		return false;
+	}
+	return true;
+}
+
+void SockAddr_In_Init(SOCKADDR_IN& serverAddr)
+{
+	//연결할 목적지 ? (IP주소 + Port)
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY); // 니가 알아서 해줘
+	serverAddr.sin_port = ::htons(7777);
+}
+
 int main()
 {
+	//Winsock 초기화
+	//관련 정보가 wsaData에 채워짐
+	WSAData wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData))
+		return 0;
 
+
+	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	Socket_Error(listenSocket);
+
+	SOCKADDR_IN serverAddr; // IPv4
+	SockAddr_In_Init(serverAddr);
+
+	//안내원 폰 개통! 식당의 대표번호
+	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	{
+		Socket_Error(listenSocket);
+	}
+
+	//영업 시작!
+	if(::listen(listenSocket, 10) == SOCKET_ERROR)
+	{
+		Socket_Error(listenSocket);
+	}
+
+	while (true)
+	{
+		SOCKADDR_IN clientAddr;
+		::memset(&clientAddr, 0, sizeof(clientAddr));
+
+		int32 addLen = sizeof(clientAddr);
+		SOCKET clientSocket = accept(listenSocket, (SOCKADDR*)&clientAddr, &addLen);
+		Socket_Error(clientSocket);
+
+		//손님 입장
+		char ipAddress[16];
+		::inet_ntop(AF_INET, &clientAddr.sin_addr, ipAddress, sizeof(ipAddress));
+		cout << "Client Connected! IP = " << ipAddress << '\n';
+	}
+
+	::WSACleanup();
+	return 0;
 }
