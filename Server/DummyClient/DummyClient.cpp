b0c7acbeb2ec65483fab3d06300821ff957bf02c
thescanthis <thescanthis@gmail.com>
2024-06-10 20:26:07 +0900

    Socket Option

--- Server/DummyClient/DummyClient.cpp
+++ Server/DummyClient/DummyClient.cpp
@@ -34,62 +34,15 @@ void SockAddr_In_Init(SOCKADDR_IN& serverAddr)
 
 int main()
 {
-	//Winsock 초기화 서버와 클라 동일함 초기화부분
-	//관련 정보가 wsaData에 채워짐
 	WSAData wsaData;
-	if (::WSAStartup(MAKEWORD(2, 2), &wsaData))
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 		return 0;
 
-#if 주석
-	클라와 서버 둘다 같음.
-	//1. 소켓을 만들어야함.
-	//소문자는 리눅스에서도 사용할수있을 가능성이 높음.
-	//ad :AF_INET = IPv4,AF_INET6=IPv6
-	//TCP = SOCK_STREAM,UDP = SOCK_DGRAM
-	//protocol = 0
-	//return : descriptor
-#endif
-	SOCKET clientSocket = ::socket(AF_INET,SOCK_DGRAM,0);
-	Socket_Error(clientSocket,"Socket");
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_DGRAM, 0);
+	if (!Socket_Error(clientSocket, "Socket"))
+		return 0;
 
-	SOCKADDR_IN serverAddr; // IPv4
-	SockAddr_In_Init(serverAddr);
-
-	while (true)
-	{
-		//클라 입장에서는 커널단계로 보내주기만 하면 성공적으로 보냈다고 생각
-		// 그래서 서버가 recv해주지않아도 넘어가는 이유를 알 수 있다.
-		//TODO
-		char snedBuffer[100] = "Hello World";
-		int32 res = ::sendto(clientSocket, snedBuffer, sizeof(snedBuffer), 0,
-			(SOCKADDR*)&serverAddr,sizeof(serverAddr));
-		if (!Socket_Error(res,"SendTo"))
-			return 0;
-
-		cout << "Send Data" <<sizeof(snedBuffer)<<'\n';
-
-		SOCKADDR_IN recvAddr;
-		::memset(&recvAddr, 0, sizeof(recvAddr));
-		int32 addLen = sizeof(recvAddr);
-
-		char recvBuffer[1000];
-
-		int32 recvLen = ::recvfrom(clientSocket, recvBuffer, sizeof(recvBuffer), 0,
-		(SOCKADDR*)&recvAddr,&addLen);
-
-
-		if (recvLen <= 0)
-		{
-			if (Socket_Error(recvLen,"RecvTo"))
-				return 0;
-		}
-		cout << "Recv Data! Data = " << recvLen << '\n';
-		cout << "Recv Data! Len = " << recvLen << '\n';
-
-		this_thread::sleep_for(1s);
-	}
 
 	::closesocket(clientSocket);
 	::WSACleanup();
-	return 0;
 }
--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -35,59 +35,49 @@ void SockAddr_In_Init(SOCKADDR_IN& serverAddr)
 
 int main()
 {
-	//Winsock 초기화
-	//관련 정보가 wsaData에 채워짐
 	WSAData wsaData;
-	if (::WSAStartup(MAKEWORD(2, 2), &wsaData))
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 		return 0;
 
-	//UDP
 	SOCKET serverSocket = ::socket(AF_INET, SOCK_DGRAM, 0);
-
-	if (!Socket_Error(serverSocket, "Socket"))
+	if(!Socket_Error(serverSocket, "Socket"))
 		return 0;
-	SOCKADDR_IN serverAddr;
-	::memset(&serverAddr, 0, sizeof(serverAddr));
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);
-	serverAddr.sin_port = ::htons(7777);
 
-	if (::bind(serverSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
-	{
-		if (!Socket_Error(serverSocket, "Bind"))
-			return 0;
-	}
+	//옵션을 해석하고 처리할 주체
+	//소켓코드 ->SOL_SOCKET
+	//IPv4 ->IPPROTO_IP
+	//TCP 프로토콜 -> IPPROTO_TCP
+	
+	//SO_KEEPALIVE = 주기적으로 연결 상태 확인 (TCP Only)
+	//상대방이 소리소문없이 연결을 끊는경우가 있음.
+	bool enable = true;
+	::setsockopt(serverSocket,SOL_SOCKET,SO_KEEPALIVE,(char*)&enable,sizeof(enable));
 
-	while (true)
-	{
-		SOCKADDR_IN clinetAddr;
-		::memset(&clinetAddr, 0, sizeof(clinetAddr));
-		int32 addLen = sizeof(clinetAddr);
+	//SO_LINGER = 지연하다
+	//소인 버퍼에 있는 데이터를 보낼것인가, 날릴것인가?
+	//onoff = 0이면 closesocket() 바로 리턴, 아니면 linger초만큼 대기
+	LINGER linger;
+	linger.l_onoff = 1;
+	linger.l_linger = 5;
+	::setsockopt(serverSocket, SOL_SOCKET, SO_LINGER, (char*)&linger, sizeof(linger));
+	
+	//Half-close
+	//::shutdown(serverSocket, SD_SEND);
+	//::closesocket(serverSocket);
+	
+	//SO_SNDBUF
+	//SO_RCVBUF
 
-		char buffer[1000] = {};
+	int32 sendBuffsize;
+	int32 optionLen = sizeof(sendBuffsize);
+	::getsockopt(serverSocket, SOL_SOCKET, SO_SNDBUF, (char*)&sendBuffsize, &optionLen);
+	
+	cout << sendBuffsize<<'\n';
 
-		int32 recvLen = ::recvfrom(serverSocket, buffer, sizeof(buffer), 0,
-			(SOCKADDR*)&clinetAddr, &addLen);
-
-		if (recvLen <= 0)
-		{
-			if (!Socket_Error(serverSocket, "RecvFrom"))
-				return 0;
-		}
-
-		cout << "Recv Data! Data=" << buffer << '\n';
-		cout << "Recv Data! Len=" << recvLen << '\n';
-
-		int32 errorCode = ::sendto(serverSocket, buffer, recvLen, 0,
-			(SOCKADDR*)&clinetAddr, sizeof(clinetAddr));
-
-		if (errorCode == SOCKET_ERROR)
-		{
-			if (!Socket_Error(serverSocket, "SendTo"))
-				return 0;
-		}
-	}
+	int32 recvBuffersize;
+	optionLen = sizeof(recvBuffersize);
+	::getsockopt(serverSocket, SOL_SOCKET, SO_RCVBUF, (char*)&recvBuffersize, &optionLen);
+	cout << recvBuffersize << '\n';
 
 	::WSACleanup();
-	return 0;
 }
