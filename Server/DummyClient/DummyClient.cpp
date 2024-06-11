1c417f82d4e58efd08f4bd2998a55e58976fa9ef
thescanthis <thescanthis@gmail.com>
2024-06-11 19:42:15 +0900

    Non-Blocking Chapter

--- Server/DummyClient/DummyClient.cpp
+++ Server/DummyClient/DummyClient.cpp
@@ -38,6 +38,78 @@ int main()
 	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 		return 0;
 
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (clientSocket == INVALID_SOCKET)
+		return 0;
+
+	u_long on = 1;
+	if (::ioctlsocket(clientSocket, FIONBIO, &on))
+		return 0;
+
+	SOCKADDR_IN serverAddr;
+	memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	::inet_pton(AF_INET, "127.0.0.1",&serverAddr.sin_addr);
+	serverAddr.sin_port = ::htons(7777);
+
+	//Connect
+	while (true)
+	{
+		if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+		{
+			//원랜 블록이여야 했는데....
+
+			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+				continue;
+
+			if (::WSAGetLastError() == WSAEISCONN)
+				break;
+			//Error;
+			break;
+		}
+	}
+	cout << "Connected to Server !"<<'\n';
+
+	char sendBuffer[100] = "Hello World";
+
+	//Send
+	while (true)
+	{
+		if (::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0) == SOCKET_ERROR)
+		{
+			if (WSAGetLastError() == WSAEWOULDBLOCK)
+				continue;
+			
+			//Error
+			break;
+		}
+		cout << "Send Data ! Len = " << sizeof(sendBuffer) << '\n';
+
+		while (true)
+		{
+			char recvBuffer[1000];
+			int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+			if (recvLen == SOCKET_ERROR)
+			{
+				if (WSAGetLastError() == WSAEWOULDBLOCK)
+					continue;
+
+				//Error
+				break;
+			}
+			else if (recvLen == 0)
+			{
+				//데이터가 아예 안옴.
+				break;
+			}
+
+			cout << "성공!" << '\n';
+			break;
+		}
+
+		this_thread::sleep_for(1s);
+	}
+
 	::closesocket(clientSocket);
 	::WSACleanup();
 }
